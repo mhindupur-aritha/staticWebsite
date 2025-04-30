16436ed01ac479f56031162b73a880405f22cba7
mhindupur <mhindupur@hotmail.com>
2025-04-30 23:32:53 +0530

    script changes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function sayHello() {
+<<<<<<< HEAD
     document.getElementById('message').innerText = "Hello from JavaScript!, Verified by Manjunath";
   }
+=======
+    document.getElementById('message').innerText = "Hello from JavaScript!, Call from javascirpt and Verified by Manjunath.H";
+  }
+>>>>>>> 0eb1f76 (script changes)
